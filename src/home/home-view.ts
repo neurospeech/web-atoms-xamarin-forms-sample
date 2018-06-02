d37e2969ffc6bc62fbb21aa4c9f3ce76961507c1
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-06-02 23:21:27 +0530

    Added easier way to add controls

--- src/home/home-view.ts
+++ src/home/home-view.ts
@@ -12,16 +12,48 @@ export class HomeView extends AtomPage {
 
         const bridge = AtomBridge.instance;
 
-        this.bind(this.element, "Title", ["title"]);
+        // this.bind(this.element, "Title", ["title"]);
 
-        const grid = bridge.create("Xamarin.Forms.Grid");
-        const label = bridge.create("Xamarin.Forms.Label");
+        // const grid = bridge.create("Xamarin.Forms.Grid");
+        // const label = bridge.create("Xamarin.Forms.Label");
 
-        bridge.appendChild(grid, label);
+        // bridge.appendChild(grid, label);
 
-        this.append(grid);
+        // this.append(grid);
 
-        this.bind(label, "Text", ["message"]);
+        // this.bind(label, "Text", ["message"]);
+
+        bridge.loadContent(this.element, `<ContentPage
+            atom:AtomBridge.Init="initPage1"
+            xmlns:atom="clr-namespace:WebAtoms;assembly=WebAtoms"
+            xmlns="http://xamarin.com/schemas/2014/forms">
+        <Grid>
+            <Grid.RowDefinitions>
+                <RowDefinition/>
+                <RowDefinition/>
+                <RowDefinition/>
+            </Grid.RowDefinitions>
+
+            <Grid.ColumnDefinitions>
+                <ColumnDefinition/>
+                <ColumnDefinition/>
+                <ColumnDefinition/>
+            </Grid.ColumnDefinitions>
+            <Label
+                atom:AtomBridge.Init="initPage2"
+                Grid.Row="1"
+                Grid.Column="1"
+            />
+        </Grid>
+    </ContentPage>`);
+    }
+
+    public initPage1(e: IAtomElement): void {
+        this.bind(e, "Title", ["title"]);
+    }
+
+    public initPage2(e: IAtomElement): void {
+        this.bind(e, "Text", ["message"]);
     }
 
 }
